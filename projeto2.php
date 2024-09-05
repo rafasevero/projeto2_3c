207df1b5a5aef0be1c45fcaf0c1a1c22acd2aedb
rafasevero <rafasevero2302@gmail.com>
2024-09-05 16:53:02 -0300

    funçcoes de venda

--- projeto2.php
+++ projeto2.php
@@ -6,10 +6,13 @@ $regPassword = '';
 $registros =[
     'rafa' => '2302'
 ];     
-$usuarioLogado = '';
+$usuarioLogado = null;
+$listaProd = [];
+$cont = 0;
+
 
 //função para escolher
-function menu($opcao){
+function start($opcao){
     if($opcao == "1"){
        login($opcao);
     }
@@ -19,28 +22,6 @@ function menu($opcao){
         echo "Erro, tente novamente\n";
     }
 }
-
-//funcao de logar
-function login($opcao){
-    global $registros;
-    global $controle;
-    $controle = '';
-    $usuario = readline("Digite seu usuário : \n");
-    $password = readline("Digite sua senha : \n");
-
-    if(array_key_exists($usuario,$registros)){
-        echo "Logado com sucesso!\n";
-        $controle = (true);
-    }
-}
-
-//funcao de deslogar
-function deslogar($controle){
-    if($controle == (true) ){
-        echo "Deslogado com sucesso!\n";
-    }
-}
-
 
 //funcao de registrar
 function registro($opcao){
@@ -60,16 +41,92 @@ function registro($opcao){
     }
     $registros [$regUsuario] = $regPassword;
     print_r($registros);
-    echo "Seu usuário foi registrado com sucesso!\n";
-   
+    echo "Seu usuário foi registrado com sucesso!\n";   
+}
 
+//funcao de logar
+function login($opcao){
+    global $registros;
+    global $controle;
+    global $usuarioLogado;
+    $controle = '';
+    $usuario = readline("Digite seu usuário : \n");
+    $password = readline("Digite sua senha : \n");
+
+    if(array_key_exists($usuario,$registros)){
+        echo "Logado com sucesso!\n";
+        $usuarioLogado = $usuario;
+        $controle = (true);
+    }
+}
+
+
+//função de vender
+function vender(){
+    global $listaProd;
+    global $cont;
+    do{
+        $produto = readline ("Qual produto você vendeu? \n");
+        $valor = readline("Valor: R$\n");
+        $voltar = readline("Deseja continuar?\n 1- SIM\n 2- NÃO \n");
+        $listaProd [$produto] = $valor;
+        $cont += $valor;
+    }while($voltar != 2);
+    print_r($listaProd); 
+    echo"O total da compra foi de R$$cont\n";  
+
+}
+
+//funcao de deslogar
+function deslogar($controle){
+    global $usuarioLogado;
+    $usuarioLogado = null;
+    echo "Deslogado com sucesso!\n";
     
 }
+
+
+//funcao de menu
+function menu(){
+    global $usuarioLogado;
+
+    echo "Bem vindo $usuarioLogado! \n ";
+    echo"1 - Vender\n";
+    echo "2 - Cadastrar novo usuário\n";
+    echo "3 - Verificar Log\n";
+    echo "4 - Deslogar\n";
+    echo "5 - Encerrar :(\n";
+    do{
+        $escolha = readline("O que você deseja fazer?\n ");
+    } while($escolha < 1 and $escolha > 4);
+
+    if($escolha == "1"){
+        vender($escolha);
+    } 
+    else if($escolha == "2" ){
+        registro($escolha);
+    }
+    else if($escolha == "3"){
+        log($escolha);
+    }
+    else if($escolha == "4"){
+        deslogar($escolha);
+    }
+}
+
+
+
+
 while(true){
-    echo "Faça o registro antes de fazer o login\n";
-    echo "O que você deseja fazer?\n";
-    $opcao = readline("1 - logar\n2 - registrar\n");
-    menu($opcao);
+    if ($usuarioLogado == null) {
+        echo "Faça o registro antes de fazer o login\n";
+        echo "O que você deseja fazer?\n";
+        $opcao = readline("1 - logar\n2 - registrar\n");
+        start($opcao);
+    }
+    else {
+        menu();
+    }
 }
 
 
